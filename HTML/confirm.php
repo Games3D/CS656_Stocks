6f0b1cb4d36c45acd34cb428fcd888f35cbb1476
Nidhi Patel <nidhipatel062194@gmail.com>
2018-02-16 12:32:17 -0500

    Add confirm.php file
    Link to confirm.php will be in the verification mail sent to the user after registration.

---
+++ HTML/confirm.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Stock Website</title>
+	<link rel="stylesheet" type="text/css" href="CSS/login.css">	
+</head>
+
+<body>
+  <div class="login-card">
+    <h1>Your account has been confirmed.</h1><br>
+	<h3>Click <a href="https://web.njit.edu/~cmb45/index.php">here</a> to log in.</h3>
+ </div>
+  <script src='http://codepen.io/assets/libs/fullpage/jquery_and_jqueryui.js'></script>
+<footer>
+
+</footer>
+</body>
+</html>
