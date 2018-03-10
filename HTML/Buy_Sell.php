788307b47522170d3267a8a8201badbdfed73bd7
Games3D <games3d8@gmail.com>
2018-03-10 14:43:12 -0500

    updates to mostly buy, sell, and banks stuff

--- HTML/Buy_Sell.php
+++ HTML/Buy_Sell.php
@@ -1,61 +1,27 @@
 <link rel="stylesheet" type="text/css" href="CSS/home.css">
 
 <?php 	
-	require_once 'DBconnect.php';
-	session_start();
+		require_once 'DBconnect.php';
+session_start();
 
-	if ($_SERVER['REQUEST_METHOD'] == 'GET') {	
-		echo("SELL!!!".$_GET['SELL_NUM']."|".isset($_GET['SELL'])."|");
-		if (isset($_GET['BUY'])){
-			echo "BUYYYYYYYYYYY";
-			//do a select on stocks to make sure the is added already, if yes then move on, if no then add
-			//insert into transactions
-			echo "S:".$_GET['symbol'];
-			echo "A:".$_GET['amount'];
-			echo "P:".$_SESSION['CURPORTFOLIO'];
-			
-			if (!isset($_SESSION['CURPORTFOLIO']) || !isset($_GET['symbol']) || !isset($_GET['amount'])){//error checking
-				$_SESSION["ERROR"] = 'Buy parameters are invalid: ' . mysql_error();
-				header('Location: Error.php');
-				return;
-			}
-			
-			//get request for stock info
-			$urlFirst = 'https://web.njit.edu/~jp834/webapps8/NewFile.jsp?OPCODE=FIRSTBUY&PARAMS='.$_GET['symbol'];
- 			$contentsFirst = file_get_contents($urlFirst);
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {	
+	//echo("SELL!!!".$_GET['SELL_NUM']."|".isset($_GET['SELL'])."|");
+	if (isset($_GET['BUY'])){
+		//echo "BUYYYYYYYYYYY";
+		//do a select on stocks to make sure the is added already, if yes then move on, if no then add
+		//insert into transactions
+		//echo "S:".$_GET['symbol'];
+		//echo "A:".$_GET['amount'];
+		//echo "P:".$_SESSION['CURPORTFOLIO'];
 
-			//If $contents is not a boolean FALSE value.
-			if($contentsFirst == false){
-				$_SESSION["ERROR"] = 'Get request error';
-				header('Location: Error.php');
-				return;
-			}
-			$DATAFIRST = explode(",", $contentsFirst);
-			
+		if (!isset($_SESSION['CURPORTFOLIO']) || !isset($_GET['symbol']) || !isset($_GET['amount'])){//error checking
+			$_SESSION["ERROR"] = 'Buy parameters are invalid: ' . mysql_error();
+			header('Location: Error.php');
+			return;
+		}
 
-			$url = 'https://web.njit.edu/~jp834/webapps8/NewFile.jsp?OPCODE=GETQUOTE&PARAMS='.$_GET['symbol'];
- 			$contents = file_get_contents($url);
-
-			//If $contents is not a boolean FALSE value.
-			if($contents == false){
-				$_SESSION["ERROR"] = 'Get request error';
-				header('Location: Error.php');
-				return;
-			}
-			$DATA = explode(",", $contents);
-			
-			
-			$FirstPrice=$DATAFIRST[10];
-			$unitPrice=$DATA[7];
-			$StockName=$DATA[2];
-			$ListPrice=$DATA[6];
-			$MarketCap=$DATA[1];
-			$OpenPrice=$DATA[3];
-			$ClosePrice=$DATA[5];
-			$Currency=$DATA[4];
-			
-			//gets the user's balance
-			$result2 = $conn->query("SELECT Balance FROM np397.SM_Portfolio where Username='".$_SESSION["USER"]."' and portfolioID='".$_SESSION['CURPORTFOLIO']."';");
+		//Check to see if the stock is valid
+			$result2 = $conn->query("SELECT count(*) as total FROM np397.SM_StockList where Symbol='".strtoupper($_GET['symbol'])."';");
 			if (!$result2) {
 				die('Invalid query: ' . mysql_error());
 				$_SESSION["ERROR"] = 'Invalid query: ' . mysql_error();
@@ -63,33 +29,88 @@
 				return;
 			}
 			$row2 = $result2->fetch_assoc();
-			$BALANCE=$row2['Balance'];
-			
-			if (($_GET['amount']*$unitPrice) > $BALANCE){
-				$_SESSION["ERROR"] = 'not enough funds to buy this stock';
+			if ($row2['total']<=0){
+				$_SESSION["ERROR"] = 'Not a valid stock';
 				header('Location: Error.php');
 				return;
 			}
+
+		//get request for stock info
+		$urlFirst = 'https://web.njit.edu/~jp834/webapps8/NewFile.jsp?OPCODE=FIRSTBUY&PARAMS='.$_GET['symbol'];
+		$contentsFirst = file_get_contents($urlFirst);
+
+		//If $contents is not a boolean FALSE value.
+		if($contentsFirst == false){
+			$_SESSION["ERROR"] = 'Get request error';
+			header('Location: Error.php');
+			return;
+		}
+		$DATAFIRST = explode(",", $contentsFirst);
+
+
+		$url = 'https://web.njit.edu/~jp834/webapps8/NewFile.jsp?OPCODE=GETQUOTE&PARAMS='.$_GET['symbol'];
+		$contents = file_get_contents($url);
+
+		//If $contents is not a boolean FALSE value.
+		if($contents == false){
+			$_SESSION["ERROR"] = 'Get request error';
+			header('Location: Error.php');
+			return;
+		}
+		$DATA = explode(",", $contents);
+
+
+		$FirstPrice=$DATAFIRST[10];
+		$unitPrice=$DATA[7];
+		$StockName=$DATA[2];
+		$ListPrice=$DATA[6];
+		$MarketCap=$DATA[1];
+		$OpenPrice=$DATA[3];
+		$ClosePrice=$DATA[5];
+		$Currency=$DATA[4];
+		
+		if ($unitPrice==0){
+			$_SESSION["ERROR"] = 'Bad Ticker';
+			header('Location: Error.php');
+			return;
+		}
 			
-			//checks to see if the stock is ther already or not
-			$result2 = $conn->query("SELECT StockID FROM np397.SM_Stocks where StockSymbol='".$_GET['symbol']."' and portfolioID='".$_SESSION['CURPORTFOLIO']."';");
-			if (!$result2) {
-				die('Invalid query: ' . mysql_error());
-				$_SESSION["ERROR"] = 'Invalid query: ' . mysql_error();
-				header('Location: Error.php');
-				return;
-			}
-			$row2 = $result2->fetch_assoc();
-			if ($row2['StockID'] != ""){//tests to see if the stock is there already or not
-				echo "|trans|";
-				$conn->query("INSERT INTO np397.SM_Transaction (StockID, ShareQuantity, UnitPrice, Timestamp) VALUES ('".$row2["StockID"]."', '".$_GET['amount']."', '".$unitPrice."', CURRENT_TIMESTAMP);");
-			}else{//new stock
-				echo "|stock|";
-				$unitPrice=$FirstPrice;//sets the price to the old price 
-				
-				$conn->query("INSERT INTO np397.SM_Stocks (PortfolioID, StockSymbol, StockName, ListPrice, MarketCap, OpenPrice, ClosePrice) VALUES ('".$_SESSION['CURPORTFOLIO']."', '".$_GET['symbol']."', '".$StockName."', '".$ListPrice."', '".$MarketCap."', '".$OpenPrice."', '".$ClosePrice."');");
-				
-				$result3 = $conn->query("SELECT StockID FROM np397.SM_Stocks where StockSymbol='".$_GET['symbol']."' and PortfolioID='".$_SESSION['CURPORTFOLIO']."';");
+		//gets the user's balance
+		$result2 = $conn->query("SELECT Balance FROM np397.SM_Portfolio where Username='".$_SESSION["USER"]."' and portfolioID='".$_SESSION['CURPORTFOLIO']."';");
+		if (!$result2) {
+			die('Invalid query: ' . mysql_error());
+			$_SESSION["ERROR"] = 'Invalid query: ' . mysql_error();
+			header('Location: Error.php');
+			return;
+		}
+		$row2 = $result2->fetch_assoc();
+		$BALANCE=$row2['Balance'];
+
+		if (($_GET['amount']*$unitPrice) > $BALANCE){
+			$_SESSION["ERROR"] = 'not enough funds to buy this stock';
+			header('Location: Error.php');
+			return;
+		}
+
+		//checks to see if the stock is ther already or not
+		$result2 = $conn->query("SELECT StockID FROM np397.SM_Stocks where StockSymbol='".$_GET['symbol']."' and portfolioID='".$_SESSION['CURPORTFOLIO']."';");
+		if (!$result2) {
+			die('Invalid query: ' . mysql_error());
+			$_SESSION["ERROR"] = 'Invalid query: ' . mysql_error();
+			header('Location: Error.php');
+			return;
+		}
+		$row2 = $result2->fetch_assoc();
+		if ($row2['StockID'] != ""){//tests to see if the stock is there already or not
+			//echo "|trans|";
+			$conn->query("INSERT INTO np397.SM_Transaction (StockID, ShareQuantity, UnitPrice, Timestamp) VALUES ('".$row2["StockID"]."', '".$_GET['amount']."', '".$unitPrice."', CURRENT_TIMESTAMP);");
+		}else{//new stock
+			//echo "|stock|";
+			$unitPrice=$FirstPrice;//sets the price to the old price 
+
+			$conn->query("INSERT INTO np397.SM_Stocks (PortfolioID, StockSymbol, StockName, ListPrice, MarketCap, OpenPrice, ClosePrice) VALUES ('".$_SESSION['CURPORTFOLIO']."', '".$_GET['symbol']."', '".$StockName."', '".$ListPrice."', '".$MarketCap."', '".$OpenPrice."', '".$ClosePrice."');");
+
+			$result3 = $conn->query("SELECT StockID FROM np397.SM_Stocks where StockSymbol='".$_GET['symbol']."' and PortfolioID='".$_SESSION['CURPORTFOLIO']."';");
 			if (!$result3) {
 				die('Invalid query: ' . mysql_error());
 				$_SESSION["ERROR"] = 'Invalid query: ' . mysql_error();
@@ -97,136 +118,200 @@
 				return;
 			}
 			$row3 = $result3->fetch_assoc();
-				
-				$conn->query("INSERT INTO np397.SM_Transaction (StockID, ShareQuantity, UnitPrice, Timestamp) VALUES ('".$row3["StockID"]."', '".$_GET['amount']."', '".$unitPrice."', CURRENT_TIMESTAMP);");
-				echo $row3["StockID"]."|";
-			}
+
+			$conn->query("INSERT INTO np397.SM_Transaction (StockID, ShareQuantity, UnitPrice, Timestamp) VALUES ('".$row3["StockID"]."', '".$_GET['amount']."', '".$unitPrice."', CURRENT_TIMESTAMP);");
+			//echo $row3["StockID"]."|";
+		}
+
+		//Updates balance
+		$result2 = $conn->query("UPDATE np397.SM_Portfolio set Balance='".($BALANCE-($_GET['amount']*$unitPrice))."' where Username='".$_SESSION["USER"]."' and portfolioID='".$_SESSION['CURPORTFOLIO']."';");
+
+		header("Location: portfolio.php");
+	} elseif (isset($_GET['sellF'])){
+		$SHARES=$_GET['SELL_NUM'];	
 		
-			//Updates balance
-			$result2 = $conn->query("UPDATE np397.SM_Portfolio set Balance='".($BALANCE-($_GET['amount']*$unitPrice))."' where Username='".$_SESSION["USER"]."' and portfolioID='".$_SESSION['CURPORTFOLIO']."';");
-			
-			header("Location: portfolio.php");
-		} elseif (isset($_GET['SELL'])){
-			//insert a sell transaction
-			//test to see if stock balance is 0, if yes then remove stock
-			
-			$SHARES=$_GET['SELL_NUM'];
-			
-			//get request for stock info
-			$result2 = $conn->query("select * from np397.SM_Stocks Where StockID='".$_GET['StockID']."';");
-			if (!$result2) {
-				die('Invalid query: ' . mysql_error());
-				$_SESSION["ERROR"] = 'Invalid query: ' . mysql_error();
-				header('Location: Error.php');
-				return;
-			}
-			$row2 = $result2->fetch_assoc();
-			
-			$url = 'https://web.njit.edu/~jp834/webapps8/NewFile.jsp?OPCODE=GETQUOTE&PARAMS='.$row2['StockSymbol'];
- 			$contents = file_get_contents($url);
-echo $contents;
-			//If $contents is not a boolean FALSE value.
-			if($contents == false){
-				$_SESSION["ERROR"] = 'Get request error';
-				header('Location: Error.php');
-				return;
-			}
-			$DATA = explode(",", $contents);
-			
-			
-			$unitPrice=$DATA[7];
-			echo $unitPrice;
-			
-			//gets the user's balance
-			$result2 = $conn->query("SELECT Balance FROM np397.SM_Portfolio where Username='".$_SESSION["USER"]."' and portfolioID='".$_SESSION['CURPORTFOLIO']."';");
-			if (!$result2) {
-				die('Invalid query: ' . mysql_error());
-				$_SESSION["ERROR"] = 'Invalid query: ' . mysql_error();
-				header('Location: Error.php');
-				return;
-			}
-			$row2 = $result2->fetch_assoc();
-			$BALANCE=$row2['Balance'];
-			
-			//gets the total shares which the current use ownes at the moment
-			$result2 = $conn->query("select sum(ShareQuantity) as aa from np397.SM_Transaction Where StockID='".$_GET['StockID']."';");
-			if (!$result2) {
-				die('Invalid query: ' . mysql_error());
-				$_SESSION["ERROR"] = 'Invalid query: ' . mysql_error();
-				header('Location: Error.php');
-				return;
-			}
-			$row2 = $result2->fetch_assoc();
-			
-			if ($row2['aa']<$SHARES || ($row2['aa']-$SHARES)<0){
-				$_SESSION["ERROR"] = 'not enough shares to sell';
-				header('Location: Error.php');
-				return;
-			}
-			
-			$conn->query("INSERT INTO np397.SM_Transaction (StockID, ShareQuantity, UnitPrice, Timestamp) VALUES ('".$_GET["StockID"]."', '-".$SHARES."', '".$unitPrice."', CURRENT_TIMESTAMP);");
-				
-			//Updates balance
-			$result2 = $conn->query("UPDATE np397.SM_Portfolio set Balance='".($BALANCE+($SHARES*$unitPrice))."' where Username='".$_SESSION["USER"]."' and portfolioID='".$_SESSION['CURPORTFOLIO']."';");
-			
-			header("Location: portfolio.php");
-			
-		} elseif ($_GET['OP']=='HISTORY'){
-			//write page to display history of transactions for this stock
-			
-			$result2 = $conn->query("SELECT * FROM np397.SM_Stocks where StockID='".$_GET["StockID"]."';");
-			if (!$result2) {
-				die('Invalid query: ' . mysql_error());
-				$_SESSION["ERROR"] = 'Invalid query: ' . mysql_error();
-				header('Location: Error.php');
-				return;
-			}
-			$row2 = $result2->fetch_assoc();
-			$Stock=$row2['StockName']." (".$row2['StockSymbol'].")";
-?>	
+		if (isset($_GET['USER']))
+			$_SESSION["USER"]=$_GET['USER'];
+		
+		if ($SHARES==-1){
+			$resultSuma = mysqli_query( $conn, "select sum(ShareQuantity) as aa from np397.SM_Transaction Where StockID='" . $_GET['StockID'] . "';" );
+			$SHARES = mysqli_fetch_assoc( $resultSuma )['aa'];
+		}
+
+		//get request for stock info
+		$result2 = $conn->query("select * from np397.SM_Stocks Where StockID='".$_GET['StockID']."';");
+		if (!$result2) {
+			die('Invalid query: ' . mysql_error());
+			$_SESSION["ERROR"] = 'Invalid query: ' . mysql_error();
+			header('Location: Error.php');
+			return;
+		}
+		$row2 = $result2->fetch_assoc();
+
+		$url = 'https://web.njit.edu/~jp834/webapps8/NewFile.jsp?OPCODE=GETQUOTE&PARAMS='.$row2['StockSymbol'];
+		$contents = file_get_contents($url);
+	//	echo $contents;
+		//If $contents is not a boolean FALSE value.
+		if($contents == false){
+			$_SESSION["ERROR"] = 'Get request error';
+			header('Location: Error.php');
+			return;
+		}
+		$DATA = explode(",", $contents);
+
+
+		$unitPrice=$DATA[7];
+	//	echo $unitPrice;
+
+		//gets the user's balance
+		$result2 = $conn->query("SELECT BankBalance FROM np397.SM_Users where Username='".$_SESSION["USER"]."';");
+		if (!$result2) {
+			die('Invalid query: ' . mysql_error());
+			$_SESSION["ERROR"] = 'Invalid query: ' . mysql_error();
+			header('Location: Error.php');
+			return;
+		}
+		$row2 = $result2->fetch_assoc();
+		$BALANCE=$row2['BankBalance'];
+
+		//gets the total shares which the current use ownes at the moment
+		$result2 = $conn->query("select sum(ShareQuantity) as aa from np397.SM_Transaction Where StockID='".$_GET['StockID']."';");
+		if (!$result2) {
+			die('Invalid query: ' . mysql_error());
+			$_SESSION["ERROR"] = 'Invalid query: ' . mysql_error();
+			header('Location: Error.php');
+			return;
+		}
+		$row2 = $result2->fetch_assoc();
+
+		if ($row2['aa']<$SHARES || ($row2['aa']-$SHARES)<0){
+			$_SESSION["ERROR"] = 'not enough shares to sell';
+			header('Location: Error.php');
+			return;
+		}
+
+		$conn->query("INSERT INTO np397.SM_Transaction (StockID, ShareQuantity, UnitPrice, Timestamp) VALUES ('".$_GET["StockID"]."', '-".$SHARES."', '".$unitPrice."', CURRENT_TIMESTAMP);");
+
+		//Updates balance
+		$conn->query("UPDATE np397.SM_Users set BankBalance='".($BALANCE+($SHARES*$unitPrice))."' where Username='".$_SESSION["USER"]."';");
+
+		//tests to see if there are anymore shares left, if not then it removes the stock
+		$resultSuma = mysqli_query( $conn, "select sum(ShareQuantity) as aa from np397.SM_Transaction Where StockID='" . $_GET['StockID'] . "';" );
+		$SHARES = mysqli_fetch_assoc( $resultSuma )['aa'];
+		
+		if ($SHARES==0){
+			$conn->query("DELETE from np397.SM_Stocks where StockID='".$_GET['StockID']."';");
+		}
+		
+		header("Location: portfolio.php");
+	}		elseif (isset($_GET['SELL'])){
+		//insert a sell transaction
+		//test to see if stock balance is 0, if yes then remove stock
+
+		$SHARES=$_GET['SELL_NUM'];	
+
+		$result2 = $conn->query("SELECT * FROM np397.SM_Stocks where StockID='".$_GET["StockID"]."';");
+		if (!$result2) {
+			die('Invalid query: ' . mysql_error());
+			$_SESSION["ERROR"] = 'Invalid query: ' . mysql_error();
+			header('Location: Error.php');
+			return;
+		}
+		$row2 = $result2->fetch_assoc();
+		$STOCKNAME=$row2['StockName']." (".$row2['StockSymbol'].")";
+		?>
 <html>
+
 <head>
- <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
- 
- 
-<meta charset="utf-8">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<link rel="stylesheet" type="text/css" href="CSS/home.css">
-	
-<title>Portfolio Page</title>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+	<meta charset="utf-8">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="CSS/home.css">
+
+	<title>Portfolio Page</title>
 </head>
 
 <body>
-<h1>Transaction Page for <?php echo $Stock?></h1>	
-<table id="projectSpreadsheet">
- <thead>
-  <tr>
-    <th>Time Stamp</th>
-    <th># of Shares</th>
-	<th>Amount</th>
-  </tr>
-	</thead>
-	<tbody id="tbodyid">
-   <?php
- 		$result=mysqli_query($conn,"select * from np397.SM_Transaction Where StockID='".$_GET["StockID"]."';");
- 		$numrows=mysqli_num_rows($result);
-
-  while($row = mysqli_fetch_assoc($result)) {?>
-  <tr>
-    <td><?php echo $row['Timestamp']?></td>
-	<td><?php echo $row['ShareQuantity']?></td>
-    <td><?php echo $row['UnitPrice']?>.</td>
-  </tr>
-   <?php }?>
-	</tbody>
-</table>
-
-<button name="HISTORY" onClick='location.href="portfolio.php"'>Back</button>
+	you are about to sell
+	<?PHP if ($SHARES!=-1)echo $SHARES; else echo "ALL";?> shares of
+	<?PHP echo $STOCKNAME?> stock, is this correct?
+	<button onClick='location.href="portfolio.php"'>Back</button>
+	<form action="Buy_Sell.php">
+		<input name="StockID" type="hidden" value="<?php echo $_GET['StockID']?>">
+		<input name="SELL_NUM" type="hidden" value="<?php echo $SHARES?>">
+		<input type="submit" name="sellF" value="Sell Stock" onclick="sellF()"/>
+	</form>
 </body>
+
 </html>
 <?php
-		}
+} elseif ( $_GET[ 'OP' ] == 'HISTORY' ) {
+	//write page to display history of transactions for this stock
+	$result2 = $conn->query( "SELECT * FROM np397.SM_Stocks where StockID='" . $_GET[ "StockID" ] . "';" );
+	if ( !$result2 ) {
+		die( 'Invalid query: ' . mysql_error() );
+		$_SESSION[ "ERROR" ] = 'Invalid query: ' . mysql_error();
+		header( 'Location: Error.php' );
+		return;
+	}
+	$row2 = $result2->fetch_assoc();
+	$Stock = $row2[ 'StockName' ] . " (" . $row2[ 'StockSymbol' ] . ")";
+	?>
+		<html>
+
+		<head>
+			<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+			<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+			<meta charset="utf-8">
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+			<link rel="stylesheet" type="text/css" href="CSS/home.css">
+
+			<title>Portfolio Page</title>
+		</head>
+
+		<body>
+			<h1>Transaction Page for <?php echo $Stock?></h1>
+			<table id="projectSpreadsheet">
+				<thead>
+					<tr>
+						<th>Time Stamp</th>
+						<th># of Shares</th>
+						<th>Amount</th>
+					</tr>
+				</thead>
+				<tbody id="tbodyid">
+					<?php
+					$result = mysqli_query( $conn, "select * from np397.SM_Transaction Where StockID='" . $_GET[ "StockID" ] . "';" );
+					$numrows = mysqli_num_rows( $result );
+
+					while ( $row = mysqli_fetch_assoc( $result ) ) {
+						?>
+					<tr>
+						<td>
+							<?php echo $row['Timestamp']?>
+						</td>
+						<td>
+							<?php echo $row['ShareQuantity']?>
+						</td>
+						<td>
+							<?php echo $row['UnitPrice']?>.</td>
+					</tr>
+					<?php }?>
+				</tbody>
+			</table>
+
+			<button name="HISTORY" onClick='location.href="portfolio.php"'>Back</button>
+		</body>
+
+		</html>
+	<?php
+}
 }
 ?>
--- HTML/portfolioEdit.php
+++ HTML/portfolioEdit.php
@@ -3,10 +3,9 @@
 	session_start();
 
 	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    echo "hiiiiiiiiiiiiiii";
 		
 		if (isset($_POST['CHANGE'])){
-			echo "CHANGE";
+			//echo "CHANGE";
 			
 			if (!isset($_SESSION['CURPORTFOLIO'])){
 				$_SESSION["ERROR"] = 'Invaild Parameters';
@@ -22,7 +21,7 @@
 			
 			$conn->query("UPDATE np397.SM_Portfolio set name='".$_POST["name"]."' Where portfolioID= '".$_SESSION['CURPORTFOLIO']."';");
 		} elseif (isset($_POST['ADD'])){
-			echo "ADD";
+			//echo "ADD";
 			
 			if (!isset($_POST["name"])){
 				$_SESSION["ERROR"] = 'Invaild Parameters';
@@ -32,17 +31,101 @@
 			
 			$conn->query("INSERT into np397.SM_Portfolio (Username, name) values('".$_SESSION["USER"]."', '".$_POST["name"]."');");
 		} elseif (isset($_POST['DELETE'])){
-			echo "DELETE";
+			//echo "DELETE";
+					?>
+<html>
+
+<head>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+	<meta charset="utf-8">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="CSS/home.css">
+
+	<title>Portfolio Page</title>
+</head>
+
+<body>
+	you are about to delete	<?PHP echo $SHARES;?> portfolio, is this correct?
+	<button onClick='location.href="portfolio.php"'>Back</button>
+	<form method = "post" action="portfolioEdit.php">
+		<input name="portfolio" type="hidden" value="<?php echo $_POST['portfolio']?>">
+		<input type="submit" name="deleteF" value="Delete Portfolio" onclick="deleteF()"/>
+	</form>
+</body>
+
+</html>
+<?php
+	return;
+			
+		}  elseif (isset($_POST['deleteF'])){
 			
 			if (!isset($_POST["portfolio"])){
 				$_SESSION["ERROR"] = 'Invaild Parameters';
 				header('Location: Error.php');
 				return;
 			}
+			//get a list of all stocks in this portfolio
+			$result = $conn->query("select * from np397.SM_Stocks Where PortfolioID='" . $_POST["portfolio"] . "';" );
+			if (!$result) {
+				die('Invalid query: ' . mysql_error());
+				$_SESSION["ERROR"] = 'Invalid query: ' . mysql_error();
+				header('Location: Error.php');
+				return;
+			}
+			while($row2 = $result->fetch_assoc()){
+				echo $row2['StockID']."|";
+				//sell all stocks one buy one
+				$url = 'https://web.njit.edu/~jp834/Buy_Sell.php?StockID='.$row2['StockID'].'&SELL_NUM=-1&sellF=Sell&USER='.$_SESSION["USER"];
+				file_get_contents($url);
+			}
 			
+			//move current portfolio money to bank
+			$result2 = $conn->query("SELECT BankBalance FROM np397.SM_Users where Username='".$_SESSION["USER"]."';");
+			if (!$result2) {
+				die('Invalid query: ' . mysql_error());
+				$_SESSION["ERROR"] = 'Invalid query: ' . mysql_error();
+				header('Location: Error.php');
+				return;
+			}
+			$row2 = $result2->fetch_assoc();
+			$BANKBALANCE=$row2['BankBalance'];
+			
+			$result2 = $conn->query("SELECT Balance FROM np397.SM_Portfolio where Username='".$_SESSION["USER"]."' and portfolioID='".$_SESSION['CURPORTFOLIO']."';");
+			if (!$result2) {
+				die('Invalid query: ' . mysql_error());
+				$_SESSION["ERROR"] = 'Invalid query: ' . mysql_error();
+				header('Location: Error.php');
+				return;
+			}
+			$row2 = $result2->fetch_assoc();
+			$BALANCE=$row2['Balance'];
+			
+			$conn->query("UPDATE np397.SM_Portfolio set Balance='0' where Username='".$_SESSION["USER"]."' and portfolioID='".$_SESSION['CURPORTFOLIO']."';");	
+			$conn->query("UPDATE np397.SM_Users set BankBalance='".($BANKBALANCE+$BALANCE)."' where Username='".$_SESSION["USER"]."';");	
+
 			$conn->query("DELETE from np397.SM_Portfolio where portfolioID='".$_POST["portfolio"]."';");
 		} elseif (isset($_POST['BALANCE_ADD'])){
-			echo "BALANCE_ADD";
+			//echo "BALANCE_ADD";
+			
+			if ($_SESSION['CURPORTFOLIO']==""){
+				$_SESSION["ERROR"] = 'No Portfolio selected';
+				header('Location: Error.php');
+				return;
+			}
+			
+			$result2 = $conn->query("SELECT BankBalance FROM np397.SM_Users where Username='".$_SESSION["USER"]."';");
+			if (!$result2) {
+				die('Invalid query: ' . mysql_error());
+				$_SESSION["ERROR"] = 'Invalid query: ' . mysql_error();
+				header('Location: Error.php');
+				return;
+			}
+			$row2 = $result2->fetch_assoc();
+			$BANKBALANCE=$row2['BankBalance'];
 			
 			$result2 = $conn->query("SELECT Balance FROM np397.SM_Portfolio where Username='".$_SESSION["USER"]."' and portfolioID='".$_SESSION['CURPORTFOLIO']."';");
 			if (!$result2) {
@@ -59,9 +142,32 @@
 				header('Location: Error.php');
 				return;
 			}
+			if ($_POST["funds"] > $BANKBALANCE){
+				$_SESSION["ERROR"] = 'Not enough funds in the bank to add';
+				header('Location: Error.php');
+				return;
+			}
 			
-			$conn->query("UPDATE np397.SM_Portfolio set Balance='".($BALANCE+$_POST["funds"])."' where Username='".$_SESSION["USER"]."' and portfolioID='".$_SESSION['CURPORTFOLIO']."';");		} elseif (isset($_POST['BALANCE_SUB'])){
-			echo "BALANCE_SUB";
+			$conn->query("UPDATE np397.SM_Portfolio set Balance='".($BALANCE+$_POST["funds"])."' where Username='".$_SESSION["USER"]."' and portfolioID='".$_SESSION['CURPORTFOLIO']."';");	
+			$conn->query("UPDATE np397.SM_Users set BankBalance='".($BANKBALANCE-$_POST["funds"])."' where Username='".$_SESSION["USER"]."';");	
+		} elseif (isset($_POST['BALANCE_SUB'])){
+			//echo "BALANCE_SUB";
+			
+			if ($_SESSION['CURPORTFOLIO']==""){
+				$_SESSION["ERROR"] = 'No Portfolio selected';
+				header('Location: Error.php');
+				return;
+			}
+			
+			$result2 = $conn->query("SELECT BankBalance FROM np397.SM_Users where Username='".$_SESSION["USER"]."';");
+			if (!$result2) {
+				die('Invalid query: ' . mysql_error());
+				$_SESSION["ERROR"] = 'Invalid query: ' . mysql_error();
+				header('Location: Error.php');
+				return;
+			}
+			$row2 = $result2->fetch_assoc();
+			$BANKBALANCE=$row2['BankBalance'];
 			
 			$result2 = $conn->query("SELECT Balance FROM np397.SM_Portfolio where Username='".$_SESSION["USER"]."' and portfolioID='".$_SESSION['CURPORTFOLIO']."';");
 			if (!$result2) {
@@ -80,8 +186,8 @@
 			}
 			
 			$conn->query("UPDATE np397.SM_Portfolio set Balance='".($BALANCE-$_POST["funds"])."' where Username='".$_SESSION["USER"]."' and portfolioID='".$_SESSION['CURPORTFOLIO']."';");
+			$conn->query("UPDATE np397.SM_Users set BankBalance='".($BANKBALANCE+$_POST["funds"])."' where Username='".$_SESSION["USER"]."';");	
 		}
 }
-
 header("Location: portfolio.php");
 ?>
