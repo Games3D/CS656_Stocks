dd31f4c1d0f4d7d4a54159188c80f55b6dcdd8d5
Games3D <games3d8@gmail.com>
2018-04-01 12:34:05 -0400

    changing , to ` and getting it to compile again

--- src/utils/StockTest.java
+++ src/utils/StockTest.java
@@ -1,7 +1,12 @@
 package utils;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io. InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.Map;
 
@@ -21,6 +26,61 @@ import org.json.simple.parser.JSONParser;
 
 @SuppressWarnings("deprecation")
 public class StockTest {
+	
+	public static class CurrencyConverter {
+		
+		private final static String USER_AGENT = "Mozilla/5.0";
+
+			/**
+			 * given 2 currencies it will ask for the current rate and return a float value
+			 *  
+			 * @param from from currency
+			 * @param to to currency
+			 * @return the conversion rate in float form
+			 */
+			public static float conversionRate(String from, String to) {
+				try {
+					String url = "https://api.fixer.io/latest?base="+from;
+
+					URL obj = new URL(url);
+					HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+					// optional default is GET
+					con.setRequestMethod("GET");
+
+					//add request header
+					con.setRequestProperty("User-Agent", USER_AGENT);
+
+					//System.out.println("\nSending 'GET' request to URL : " + url);
+					//System.out.println("Response Code : " + con.getResponseCode());
+
+					BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+					String inputLine;
+					StringBuffer response = new StringBuffer();
+
+					while ((inputLine = in.readLine()) != null) {
+						response.append(inputLine);
+					}
+					in.close();
+
+					String reply=response.toString().substring(1);
+					String[] rates=reply.substring(reply.indexOf("{")+1, reply.length()-2).split(",");
+					String d=rates[0].substring(1, 4);
+					for (String cur:rates) {
+						if (cur.substring(1, 4).equals(to))
+							return Float.parseFloat(cur.substring(6));
+					}
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
+				} catch (ProtocolException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				return -1;
+			}
+	}
+	
 	String Crumb="", Symbol="";
 	HttpClient client = HttpClientBuilder.create().build();
 	HttpClientContext context = HttpClientContext.create();
@@ -78,14 +138,15 @@ public class StockTest {
 				while((bytesRead = bis.read(contents)) != -1) { 
 					strFileContents += new String(contents, 0, bytesRead);              
 				}
-				String[] line=strFileContents.split(",");
-				String[] quote=getQuote().split(",");
+				strFileContents=strFileContents.replaceAll(",", "`");//replaces the , with `
+				String[] line=strFileContents.split("`");
+				String[] quote=getQuote().split("`");
 				
 				 String cc=line[10];
 			     if(!quote[4].equals("USD")) 
 			       	cc=String.valueOf(CurrencyConverter.conversionRate(quote[4], "USD")*Float.parseFloat(cc));
 				
-			     strFileContents+=","+cc;
+			     strFileContents+="`"+cc;
 			     
 				bis.close();
 			}
@@ -129,13 +190,13 @@ public class StockTest {
 		        	cc=String.valueOf(CurrencyConverter.conversionRate((String)DATA.get("currency"), "USD")*Float.parseFloat(cc));
 		        
 				out=new String(
-				DATA.get("symbol")+ "," +
-				DATA.get("marketCap")+ "," +
-				DATA.get("longName")+ "," +
-				DATA.get("regularMarketOpen")+ "," +
-				DATA.get("currency")+ "," +
-				DATA.get("regularMarketPreviousClose")+ "," +
-				DATA.get("regularMarketPrice")+ "," +
+				DATA.get("symbol")+ "`" +
+				DATA.get("marketCap")+ "`" +
+				DATA.get("longName")+ "`" +
+				DATA.get("regularMarketOpen")+ "`" +
+				DATA.get("currency")+ "`" +
+				DATA.get("regularMarketPreviousClose")+ "`" +
+				DATA.get("regularMarketPrice")+ "`" +
 				cc);
 				
 				//System.out.println(DATA.get("market"));
