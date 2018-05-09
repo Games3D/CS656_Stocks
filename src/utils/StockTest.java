4d49673a1d71e159cc9f1dcaa898a05d62b853f9
Games3D <games3d8@gmail.com>
2018-05-09 01:12:55 -0400

    stuff

--- src/utils/StockTest.java
+++ src/utils/StockTest.java
@@ -323,7 +323,7 @@ public class StockTest {
 			"\n"+
 			"int Q1,Q2,Q3,Q4,Q5,Q6,Q7,Q8,Q9,Q10;";
 			
-			//connecting to the AFS
+			//connecting to the AFS upload
 			String host="afs1.njit.edu";
 			String user="jp834";
 			String password="Mynjit19";
