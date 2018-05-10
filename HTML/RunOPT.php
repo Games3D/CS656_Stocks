4e31fdb88f90bfef3006f1faf76f83ced85b04d1
Games3D <games3d8@gmail.com>
2018-05-10 19:54:21 -0400

    run opt change

--- HTML/RunOPT.php
+++ HTML/RunOPT.php
@@ -5,29 +5,57 @@ session_start();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   //get request for stock info
-$url = "https://web.njit.edu/~jp834/webapps8/NewFile.jsp?OPCODE=AFS&PARAMS=0.005045997,51.875423 ,1.073132221,-0.000995944,187.36 ,1.066861448,0.003802902,100.5 ,0.984216143,0.000751952,344.5 ,0.690125203,0.003323035,152.61 ,1.098406147,0.004520559,14.62 ,1.025943826,0.000442582,142.61 ,0.717279322,0.000217747,203.42 ,0.883875360,0.006611401,51.875423 ,0.993940550,0.003667151,8.793088 ,0.743267272,2639.963934,1.2";
+$url = "https://web.njit.edu/~jp834/webapps8/NewFile.jsp?OPCODE=AFS&PARAMS=";
 //$url="https://web.njit.edu/~jp834/webapps8/NewFile.jsp?OPCODE=FIRSTBUY&PARAMS=ko";
 $result = $conn->query("SELECT * FROM `SM_StockList`
 						JOIN `SM_Stocks` ON SM_Stocks.StockSymbol = SM_StockList.Symbol
 						JOIN `SM_Portfolio` ON SM_Portfolio.portfolioID = SM_Stocks.PortfolioID
 						WHERE SM_Portfolio.Username = '".$_SESSION["USER"]."' AND SM_Portfolio.portfolioID = '".$_SESSION['CURPORTFOLIO']."' ORDER BY Market,Symbol ASC;");
-/*
+
 $counter=0;
 
 while($row = $result->fetch_assoc()){
 	if($row["ER"] == null){
-		$urlFirst .= "0,";
+		$url .= "0,";
     } else {
-		$urlFirst .= $row["ER"].','.$_SESSION['finalarray'][$counter].','.$row["Beta"].',';
+		$url .= round($row["ER"],2).','.round($_SESSION['finalarray'][$counter],2).','.round($row["Beta"],2).',';
     	$counter=$counter+1;
 	}
 }
 
-$urlFirst.=$_SESSION['TOTALPORT'].','.$_POST['betaopt'];*/
+$urlFirst.=$_SESSION['TOTALPORT'].','.$_POST['betaopt'];
 echo $url;
 
 $contents = file_get_contents($url);
 echo $contents;
+	
+	
+	
+	
+	
+	
+	
+// Get cURL resource
+$curl = curl_init();
+// Set some options - we are passing in a useragent too here
+curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => $url,
+    CURLOPT_USERAGENT => 'Codular Sample cURL Request'
+));
+// Send the request & save response to $resp
+$resp = curl_exec($curl);
+echo "CURL:".$resp;
+// Close request to clear up some resources
+curl_close($curl);
+	
+	
+	
+	
+	
+	
+	
+	
 
 //If $contents is not a boolean FALSE value.
 if($contents == false){
